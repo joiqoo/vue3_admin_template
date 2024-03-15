0764f2f2da7d2069e72af2f0dc6210ce6dbcf3ab
--global <tinghao_zhao@brown.edu>
2024-03-15 15:19:13 +0900

    chore: import Element Plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {Plus} from '@element-plus/icons-vue'
+</script>
 
 <template>
   <div>
     <h1>this is app root component</h1>
+    <el-button type="primary" size="default" :icon="Plus"> button</el-button>
+    <el-pagination background layout="prev, pager, next" :total="1000" />
+    
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//import element-plus plugin and css style
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+// get the app instance object
+const app = createApp(App)
+// install element-plus plugin
+app.use(ElementPlus)
+// mout the app
+app.mount('#app')
