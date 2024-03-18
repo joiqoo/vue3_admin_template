9ca04c60345001dfaa45c1af90c021238ee401c2
--global <tinghao_zhao@brown.edu>
2024-03-18 16:24:20 +0900

    feat: mock data and interface

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,12 @@
 import SvgIcon from './SvgIcon/index.vue'
 import Pagination from './Pagination/index.vue'
 
-const allGlobalComponent:any = { SvgIcon, Pagination };
+const allGlobalComponent: any = { SvgIcon, Pagination }
 export default {
-    install(app:any) {
-        Object.keys(allGlobalComponent).forEach(key => {
-            // register global components
-            app.component(key, allGlobalComponent[key])
-        })
-    }
+  install(app: any) {
+    Object.keys(allGlobalComponent).forEach((key) => {
+      // register global components
+      app.component(key, allGlobalComponent[key])
+    })
+  },
 }
