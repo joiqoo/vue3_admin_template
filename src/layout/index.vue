e564b752fe27a96abc5cb61207b2635bfd79e16f
--global <tinghao_zhao@brown.edu>
2024-03-22 14:50:26 +0900

    feat: layout component

---
+++ src/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="layout_container">
+        <div class="layout_slider">123</div>
+        <div class="layout_tabbar">456</div>
+        <div class="layout_main">
+            <p style="height: 5000px; background: green;">this is a paragraph</p>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.layout_container {
+    width: 100%;
+    height: 100vh;
+
+    .layout_slider{
+        width: $base-menu-width;
+        height: 100vh;
+        background: $base-menu-background;
+    }
+    .layout_tabbar{
+        position: fixed;
+        width: calc(100% - $base-menu-width);
+        height:$base-tabbar-height;
+        background: cyan;
+        top: 0px;
+        left: $base-menu-width; 
+    }
+    .layout_main{
+        position: absolute;
+        width: calc(100% - $base-menu-width);
+        height: calc(100vh - $base-tabbar-height);
+        background: yellow;
+        left: $base-menu-width;
+        top: $base-tabbar-height;
+        padding: 20px;
+        overflow: auto;
+    }
+
+}
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,17 +3,36 @@
     <el-row>
       <el-col :span="12" :xs="0">left</el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>Hello</h1>
           <h2>Welcome to atGuigu Background Management System</h2>
           <el-form-item prop="username">
-            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            ></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button :loading="loading" class="login_btn" type="primary" size="default" @click="login">
+            <el-button
+              :loading="loading"
+              class="login_btn"
+              type="primary"
+              size="default"
+              @click="login"
+            >
               login
             </el-button>
           </el-form-item>
@@ -32,14 +51,13 @@ import { ElNotification } from 'element-plus'
 import { getTime } from '@/utils/time'
 
 let useStore = useUserStore()
-let loginForms = ref();
+let loginForms = ref()
 let $router = useRouter()
 let loading = ref(false)
 
 let loginForm = reactive({ username: 'admin', password: '111111' })
 const login = async () => {
-  await loginForms.value.validate();
-
+  await loginForms.value.validate()
 
   loading.value = true
   try {
@@ -62,16 +80,16 @@ const login = async () => {
 
 const validatorUserName = (rule: any, value: any, callback: any) => {
   if (/^.{5,10}$/.test(value)) {
-    callback();
+    callback()
   } else {
-    callback(new Error('username should contain between 5 and 10 characters'));
+    callback(new Error('username should contain between 5 and 10 characters'))
   }
 }
 const validatorPassword = (rule: any, value: any, callback: any) => {
   if (/^.{6,15}$/.test(value)) {
-    callback();
+    callback()
   } else {
-    callback(new Error('password should contain between 6 and 15 characters'));
+    callback(new Error('password should contain between 6 and 15 characters'))
   }
 }
 
@@ -79,12 +97,12 @@ const rules = {
   username: [
     // {required: true, message:'user name cannot be null.', trigger: 'blur'},
     // {required: true, min: 6, max: 10, message:'the length should be between 6 and 10 chars.', trigger: 'change'}
-    { trigger: 'change', validator: validatorUserName }
+    { trigger: 'change', validator: validatorUserName },
   ],
   password: [
     // {required: true, min: 6, max: 15, message:'the length should be between 6 and 15 chars.', trigger: 'change'}
-    { trigger: 'change', validator: validatorPassword }
-  ]
+    { trigger: 'change', validator: validatorPassword },
+  ],
 }
 </script>
 
